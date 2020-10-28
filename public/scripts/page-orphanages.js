8183c97ae4f4fe3317833824de469dd56049cacb
kev <kevingullytps@gmail.com>
2020-10-27 22:27:09 -0300

    NLW#3 workshop 04 finished

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,33 +1,48 @@
-//create map
+// const { orphanages } = require("../../src/pages");
 
-const mymap = L.map('mapid').setView([-23.5213167,-46.7295751], 15);
+//create map
+const map = L.map('mapid').setView([-23.5213167,-46.7295751], 15);
 
 //create and add tileLayer
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29, 68],
     popupAnchor: [170, 2]
 
 })
 
-// create popap Overlay
 
-const popup = L.popup({
-    closeButton: false,
-    className: "map-popup",
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" </a>')
 
-// create and add marker
+function addMarker({id, name, lat, lng}) {
 
-L.marker([-23.5213167,-46.7295751], { icon })
-.addTo(mymap)
-.bindPopup(popup)
-.openPopup();
+    // create popap Overlay
+    const popup = L.popup({
+        closeButton: false,
+        className: "map-popup",
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name} <a href="/orphanage?id=${id}"><img src="/images/arrow-white.svg" </a>`)
+
+    // create and add marker
+    L.marker([lat,lng], { icon })
+    .addTo(map)
+    .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+
+orphanagesSpan.forEach( span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+
+    addMarker(orphanage)
+}) 
